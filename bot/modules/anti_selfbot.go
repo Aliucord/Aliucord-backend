85972f5ff5006412e61ad491f687b50e0fb2d0c5
Juby210 <31005896+Juby210@users.noreply.github.com>
2021-12-22 01:56:54 +0100

    fix condition in anti selfbot

--- bot/modules/anti_selfbot.go
+++ bot/modules/anti_selfbot.go
@@ -18,7 +18,7 @@ func initAntiSelfbot() {
 	s.AddHandler(func(msg *gateway.MessageCreateEvent) {
 		if !msg.Author.Bot {
 			for _, e := range msg.Embeds {
-				if e.Type == discord.NormalEmbed && e.URL != "" {
+				if e.Type == discord.NormalEmbed && e.URL == "" {
 					logger.LogIfErr(s.Ban(msg.GuildID, msg.Author.ID, api.BanData{
 						AuditLogReason: "sent selfbot embed (" + api.AuditLogReason(msg.URL()) + ")",
 					}))
